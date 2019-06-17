b65eb14518590f29b1476b53e0548c167b069704
312639 <47399971+312639@users.noreply.github.com>
2019-06-17 18:37:54 -0400

    files Uploaded

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿/* Cameron Heinz
+ * Project Euler Problem # 31
+ * Coin Sums
+ * June 17,2019*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _312639CoinSums
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int target = 200;
+            int ways = 0;
+
+            for (int a = target; a >= 0; a -= 200)
+            {
+                for (int b = a; b >= 0; b -= 100)
+                {
+                    for (int c = b; c >= 0; c -= 50)
+                    {
+                        for (int d = c; d >= 0; d -= 20)
+                        {
+                            for (int e = d; e >= 0; e -= 10)
+                            {
+                                for (int f = e; f >= 0; f -= 5)
+                                {
+                                    for (int g = f; g >= 0; g -= 2)
+                                    {
+                                        ways++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            MessageBox.Show(ways.ToString());
+        }
+    }
+}
